ada11eaf95930a6bdedc3737ddf87f57157f9cee
T3jota <80195312+T3jota@users.noreply.github.com>
2021-11-04 11:50:31 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function formChecker(){
+	event.preventDefault();
+	document.getElementById("myForm").style.display = "none";
+	var name=document.getElementById("formName").value;
+	var email=document.getElementById("formEmail").value;
+	alert(name+", thank you for your details. We will be in touch via "+email+" shortly.");
+}
+
